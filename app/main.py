d12cc73c0d1ed3ccf8b3a880d86b2bdf8ae46ddd
shamikhan005 <shamikhan8595@gmail.com>
2026-02-17 18:51:35 +0530

    CORS

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.schemas import ScenarioInput
 from app.orchestrator import run_full_analysis
 
 app = FastAPI(title="nz-sim")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.post("/analyze")
 def analyze(input_data: ScenarioInput):
